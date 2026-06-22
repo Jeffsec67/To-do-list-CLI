3d728be1c4ee179b77dc07bda3941257e03d7845
Jeffsec67 <jjbeaumier1@gmail.com>
2026-06-21 21:23:55 -0400

    Initial commit

--- To-do list CLI.py
+++ To-do list CLI.py
@@ -7,44 +7,24 @@ To-do list CLI
 #Separate functions for adding, listing, completing, 
 # and deleting. 
 
-def load_tasks(filename="tasks.json") :
-#the function has a default parameter if undefined, 
-# retains user input for existing file option
-#try to open file if breaks,
-# except SomeError:do this if it breaks with this error    
+def load_tasks(filename="tasks.json") : 
     tlist = list()
     try :
-        with open(filename, "r") as f :
-            for line in f : 
-                if "." in line and "-" in line : 
-            #####if file is in "1. Task - Done" format#####
-                    number, rest = line.split(".", 1) 
-            #get number from rest of line, 
-            # and 1 is how many splits you need
-            # number gets the first half
-            # rest gets the second half
-            # "1." is now gone from the line
-                    task, status = rest.split("-", 1)
-            #split the REST of the line (no more "1.")
-            # task gets first half, status the second
-                    number, task, status = number.strip(), task.strip(), status.strip()
-            #removes spaces from front and back " Hello "
-                    fulltask = {"number_key": number, "task_key": task, "status_key": status}
-                    tlist.append(fulltask)
-            #all task contents go into their own dict
-            #all dictionaries go into one list
+        with open(filename, "r") as f : 
+            tlist = json.load(f)
+            #json.load() gives every line as a dictionary
     except FileNotFoundError : 
         tlist = list() 
-    return tlist
-    #make tlist accessable outside the function
+    
+    return tlist       
 
-def save_tasks() : 
-#load file and append it
-        
+def save_tasks(tasks, filename="tasks.json") : 
+#load file and append it    
+#take new task and json.dump
+     with open(filename, "w") as f :
+         json.dump(tasks, f)
 
-
-
-
+def add_tasks() :
 
 filename = input("Enter filename: ")
 tasks = load_tasks(filename)
